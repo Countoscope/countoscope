53e0b64386c12577c6dfeb888d4a97d22b1c2a8e
Simon Gravelle <simon.gravelle@live.fr>
2024-07-11 16:36:12 +0200

    added box and trajectory reader

--- countoscope/box.py
+++ countoscope/box.py
@@ -6,9 +6,59 @@
 #
 # Released under MIT Licence
 """Module for preparing and cutting the box."""
+from trajectory import Trajectory
+import numpy as np
 
 
-class Box:
+class Box(Trajectory):
+    r"""Class providing options for dividing the system into small boxes.
+    
+    The box size can be provided as an integer or an array of same 
+    size as the system dimension. 
+    """
+
     def __init__(self,
+                 box_size: None,
+                 *args,
+                 **kwargs
                  ):
-        pass
+        super().__init__(*args, **kwargs)
+        self.box_size = box_size
+
+    def run(self):
+        self.import_trajectory()
+        self.read_information_trajectory()
+        self.detect_system_dimension()
+        self.specify_box_size()
+
+    def specify_box_size(self):
+        """Calculate the measurement box size in all N dimensions"""
+        if np.shape(self.box_size) == ():
+            # Box size was provided as an integer, use its value to make a
+            # cube or a square
+            self.box_size = np.zeros(self.dimension)+self.box_size
+        # make sure that the box size and the dimension of the system are consistent
+        assert np.shape(self.box_size)[0] == self.dimension, \
+            """ERROR: Inconsistent box size and system dimension."""
+
+    def select_the_slices():
+        assert len(l0_box) == dimensions, """WARNING: unexpected number of values in l0_box, give 3 (x, y, and z)"""
+        bounds = []
+        new_l0 = []
+        for i, dir in zip(range(dimensions), ["x", "y", "z"]):
+            lower_boundary, higher_boundary = domaine_boundaries[i]
+            bounds_i = np.arange(lower_boundary, higher_boundary+l0_box[i], l0_box[i])
+            if np.round(bounds_i[-1],3) > np.round(higher_boundary,3):
+                print("WARNING -- Wrong slicing of the system in the direction", dir)
+                print("\t The simulation box with boundaries:", lower_boundary, higher_boundary)
+                print("\t can't be sliced by blocks of", l0_box[i])
+                closer_n_slice = np.int32(((higher_boundary-lower_boundary))/l0_box[i])
+                bounds_i = np.linspace(lower_boundary, higher_boundary, num=closer_n_slice)
+                l0 = np.round(np.diff(bounds_i)[0],5)
+                new_l0.append(l0)
+                print("\t", l0, "is used instead")
+            else:
+                new_l0.append(l0_box[i])
+            bounds.append(bounds_i)
+        l0_box = np.array(new_l0)
+        bounds = bounds
--- countoscope/trajectory.py
+++ countoscope/trajectory.py
@@ -6,20 +6,78 @@
 #
 # Released under MIT Licence
 """Module for preparing the trajectories."""
+import os
 import numpy as np
 import MDAnalysis as mda
+from chemfiles import Trajectory as chem_traj
 
 
 class Trajectory:
+    r"""Class providing options for importing the trajectory."""
+
     def __init__(self,
-                 trajectory: np.ndarray = None,
-                 universe: mda.Universe = None,
+                 trajectory_file: str,
+                 topology_file: str = None,
+                 system_size: np.array = None,
+                 dimension: int = None,
                  *args,
                  **kwargs
                  ):
         super().__init__(*args, **kwargs)
-        self.trajectory = trajectory,
-        self.universe = universe,
-    def run(self):
-        print(self.trajectory)
-        print(self.universe)
+        self.trajectory_file = trajectory_file
+        self.topology_file = topology_file
+        self.system_size = system_size
+        self.dimension = dimension
+
+    def import_trajectory(self):
+        """Import trajectory file using Chemfiles"""
+        # Make sure that the trajectory file exists.
+        assert os.path.exists(self.trajectory_file),  \
+            """Error: Trajectory not found."""
+        # Import the trajectory file using Chemfiles.
+        self.trajectory = chem_traj(self.trajectory_file)
+        # If provided, add the topology to the trajectory.
+        if self.topology_file is not None:
+            self.trajectory.set_topology(self.topology_file)
+
+    def read_information_trajectory(self):
+        """Read basic information from trajectory"""
+        self.nb_steps = self.trajectory.nsteps
+        # If box_size was not provided, try reading it from trajectory.
+        if self.system_size is None:
+            self.system_size = np.array(self.trajectory.read_step(0).cell.lengths)
+        # If box_size is 0, return a warning
+        assert np.sum(np.array(self.system_size)) > 0, \
+            """Error: No box size in trajectory, provide system_size"""
+        
+    def detect_system_dimension(self):
+        """Attempt to guess the dimension from the trajectory.
+        
+        In case the dimension was entered by the user, make sure its value is
+        consistent.
+        """
+        if self.dimension is None:
+            # Measure the number of column of the particle positions
+            number_columns = np.shape(self.trajectory.read_step(0).positions)[1]
+            if number_columns == 2:
+                # This may not be a possibility.
+                dimension = 2
+            elif number_columns == 3:
+                if np.sum(self.trajectory.read_step(0).positions[:,2]**2) == 0:
+                    # All the z-coordinate are 0, then its assumed that the simulation is 2D
+                    dimension = 2
+                    print("INFO: the trajectory was detected as 2D. If it is "
+                        "a mistake, provide dimension.")
+                else:
+                    dimension = 3
+            else:
+                print("Wrong number of coordinate in the trajectory."
+                      "Must be 2 or 3.")
+            self.dimension = dimension
+        elif self.dimension == 3:
+            if np.sum(self.trajectory.read_step(0).positions[:,2]**2) == 0:
+                print("INFO: the selected dimension is 3 "
+                    "but the particule coordinates seem to be 0 along z.")
+        else:
+            assert (self.dimension == 2) | (self.dimension == 3), \
+                """ERROR: Unsuported dimension. Must be 2 or 3."""
